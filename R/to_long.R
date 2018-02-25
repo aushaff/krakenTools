cafa14b0eb20973f78be4d8417bb36f63c51616e
aushaff <aushaff@zoho.eu>
2018-02-25 19:15:13 +0100

    to_long completed and tested on XBTEUR. Needs screen prints for progress

--- R/to_long.R
+++ R/to_long.R
@@ -9,73 +9,83 @@ to_long <- function(folder_root, in_curr) {
   dp_dir <- file.path(folder_root, in_curr, "data_products")
   
   all_files <- gtools::mixedsort(list.files(raw_dir))
-  return(all_files)
-  
+
   # check to see if long_df exists
-  long_filename <- paste0(in_curr, "_long_all_data.csv")
+  long_filename <- paste0(in_curr, "_long_all_tick_data.csv")
   
   file_exists <- file.exists(file.path(dp_dir, long_filename))
   
   # if doesnt exist:
   if(!file_exists) {
+  
+    # take the earliest file from all files
+    curr_file <- min(all_files)
     
-    curr_files <- all_files
+    # read it in 
+    new_long_df <- fread(file.path(raw_dir, curr_file))
+    new_long_df$last <- strsplit(curr_file, "_")[[1]][1]
     
-    # do.call rbind curr_files
-    new_long_df <- do.call(rbind, lapply(curr_files, 
-                                         function(x) {
-                                              read.csv(file.path(raw_dir, x),
-                                                       header = TRUE)
-                                         }))
-  } else { # if does exist 
+    # save it 
+    fwrite(new_long_df, file.path(dp_dir, long_filename),
+           append = FALSE,
+           sep = ";",
+           row.names = FALSE,
+           col.names = TRUE)
     
-    # read in the existing long_df and catch an error
-    tryCatch({
+    cat("New long file created", "\n")
+  } 
+  
+  # read in the existing long_df and catch an error
+  tryCatch({
       
-      old_long_df <- read.csv(file.path(raw_dir, long_filename),
-                              header = TRUE)  
-    }, warning = function(warn) {
-      print(warn)
-    }, error = function(err) {
-      print(paste0("error reading existing long_df ", long_filename))
-    })
+    old_long_df <- fread(file.path(dp_dir, long_filename))
+  }, warning = function(warn) {
+    print(warn)
+  }, error = function(err) {
+    print(paste0("error reading existing long_df ", long_filename))
+  })
    
-    # get latest_date in the existing long_df
-    rec_date <- min(old_long_df$unix_time)  
+  # get latest_date in the existing long_df
+  rec_date <- as.numeric(max(old_long_df$last)  )
     
-    # select filenames from all_data that have later 
-    # date than rec_date
+  # split the 'lasts' from all the file names
+  lasts <- strsplit(all_files, "_")
+  lasts_pt1_n <- as.numeric(unlist(lasts)[seq(1, length(lasts)*7, 7)])
+  
+  # make current only those filenames that are later than
+  # the latest date in the long dataframe
+  curr_files <- all_files[rec_date < lasts_pt1_n]
+  
+  if(length(curr_files)==0) {
     
-    curr_files <- all_files[all_files > rec_date]
-    
-    # do.call rbind curr_files
-    new_long_df <- do.call(rbind, lapply(curr_files, 
-                                         function(x) {
-                                           read.csv(file.path(raw_dir, x),
-                                                    header = TRUE)
-                                         }))
-    
+    cat("All up to date", "\n")  
+    return(0)
     
   }
   
-  
-  
-      
-  
-  
-    
-    
-  
-    # do.call rbind curr_files
-    # rbind to existing long_df
-  
-  
-  # check for duplicate rows - remove
-  
-  # check for missing dates
-  
-  # back and forwards fill missing dates - add comment column
-  
+  # append the curr_files to the existing dataframe
+  sapply(curr_files, 
+           function(curr_file) {
+             in_file <- fread(file.path(raw_dir, curr_file))
+             in_file$last <- strsplit(curr_file, "_")[[1]][1]
+             
+             # check to ensure that we are adding data of the
+             # same dimensions
+             stopifnot(ncol(in_file)==8)
+             
+             # then write
+             fwrite(in_file, file.path(dp_dir, long_filename),
+                    append = TRUE,
+                    sep = ";",
+                    row.names = FALSE,
+                    col.names = FALSE,
+                    verbose = TRUE)
+             })
+
+  # load in file and check for (and remove) duplicates
+  long_file <- fread(file.path(dp_dir, long_filename),
+                     sep = ";")
+  return(anyDuplicated(long_file))
   
 }
 
--- r_access_scripts/call_to_long.R
+++ r_access_scripts/call_to_long.R
@@ -1,11 +1,24 @@
 
 # Home PC
-#folder_root <- "/media/deckard/External/data/kraken"
+folder_root <- "/media/deckard/External/data/kraken"
 # G Laptop - this needs to be mounted first
-folder_root <- "/Volumes/NTFS/data/kraken"
+#folder_root <- "/Volumes/NTFS/data/kraken"
 list.files(folder_root)
 
-curr <- "XBTEUR"
-list.files(file.path(folder_root, curr, "raw_data"))
-t <- to_long(folder_root, curr)
+in_curr <- "XBTEUR"
+list.files(file.path(folder_root, in_curr, "raw_data"))
+to_long(folder_root, in_curr)
+
 max(t)
+
+x <- curr_files[1]
+library(data.table)
+
+system.time(read.csv(file.path(raw_dir, x),
+         header = TRUE))
+# user  system elapsed 
+# 0.015   0.001   0.022 
+
+system.time(t <- fread(file.path(raw_dir, x)))
+# user  system elapsed 
+# 0.006   0.000   0.007
