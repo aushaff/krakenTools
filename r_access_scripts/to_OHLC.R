53b56f616d1d0bfb6e5eee3332d0ff79c278eebd
austin.haffenden <austin.haffenden@irstea.fr>
2018-03-06 15:30:15 +0100

    from long_sub to xts and plotted working test case

--- r_access_scripts/to_OHLC.R
+++ r_access_scripts/to_OHLC.R
@@ -8,7 +8,8 @@ to_OHLC <- function(in_curr, folder_root, period) {
 }
 
 in_curr <- c("XBTEUR")
-folder_root <- "/media/deckard/External/data/kraken"
+#folder_root <- "/media/deckard/External/data/kraken"
+folder_root <- getwd()
 dp_dir <- file.path(folder_root, in_curr, "data_products")  
 long_filename <- paste0(in_curr, "_long_all_tick_data.csv")
 
@@ -18,20 +19,14 @@ system.time(long_file <- data.table::fread(file.path(dp_dir, long_filename),
 #   user  system elapsed 
 # 59.930   3.770  83.678 
 
-long_sub <- as.matrix(long_file[1:10,])
+long_sub <- data.table::fread(file.path("data", "long_sub.csv"),
+                              stringsAsFactors = FALSE,
+                              dec = ".")
+myxts <- xts(long_sub[, c("price", "volume")], 
+             order.by = anytime::anytime(long_sub$unix_time))
 
-system.time(long_sub$t <- as.POSIXct(long_sub$time, "UTC", "%Y-%m-%d %H:%M:%S"))
-# user  system elapsed 
-# 0.005   0.000   0.048 
-long_sub
+plot(myxts)
 
-library(xts)
-help(xts)
-myxts <- as.xts(long_sub[, c("price", "volume")], order.by = time)
-
-long <- to_OHLC("XBTEUR", folder_root)
-cp_long <- long
-cp_long$time <- as.POSIXct(cp_long$time, "UTC", "%Y%m%dT%H%M%S")
 
 
 
