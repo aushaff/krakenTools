ba43163a00823d26c806459c512851c2457110ec
aushaff <aushaff@zoho.eu>
2018-03-08 23:32:17 +0100

    XBTEUR to min/hour/day OHLC

--- r_access_scripts/to_OHLC.R
+++ r_access_scripts/to_OHLC.R
@@ -32,12 +32,49 @@ system.time(long_file <- data.table::fread(file.path(dp_dir, long_filename),
 
 system.time(long_myxts <- xts(long_file[, c("price", "volume")],
              order.by = anytime::anytime(long_file$unix_time)))
+# user  system elapsed 
+# 1.921   4.085  77.792 
 
 #saveRDS(long_sub, file.path(dp_dir, "delete_long_sub.rds"))
 system.time(saveRDS(long_myxts, file.path(dp_dir, "XBTEUR_long_xts.rds")))
+# user  system elapsed 
+# 35.529   0.704  48.029 
 
+# to
+# xts_sub <- long_myxts[1:100,]
 
+# to OHLC: 1, 5, 15, 30 min; 1, 4 and 24 hours
+# one_min <- to.minutes(xts_sub, k = 1)
+# min <- to.minutes(long_myxts[1000:1010,], k = 5)
 
+# to 1 minute
+one_min <-  to.minutes(long_myxts, k = 1)
+system.time(saveRDS(one_min, file.path(dp_dir, "XBTEUR_OHLC_one_min_xts.rds")))
 
+# to 5 minute
+five_min <-  to.minutes5(long_myxts)
+system.time(saveRDS(five_min, file.path(dp_dir, "XBTEUR_OHLC_five_min_xts.rds")))
 
+# to 15 minute
+fifteen_min <-  to.minutes15(long_myxts)
+system.time(saveRDS(fifteen_min, file.path(dp_dir, "XBTEUR_OHLC_15_min_xts.rds")))
 
+# to 30 minute
+thirty_min <-  to.minutes30(long_myxts)
+system.time(saveRDS(thirty_min, file.path(dp_dir, "XBTEUR_OHLC_30_min_xts.rds")))
+
+# to 1 hour
+one_hour <-  to.hourly(long_myxts)
+system.time(saveRDS(one_hour, file.path(dp_dir, "XBTEUR_OHLC_one_hour_xts.rds")))
+
+# to 4 hour
+four_hour <-  to.period(long_myxts, period = "hours", k = 4)
+system.time(saveRDS(four_hour, file.path(dp_dir, "XBTEUR_OHLC_four_hours_xts.rds")))
+
+# to 24 hour
+one_day <-  to.daily(long_myxts)
+system.time(saveRDS(one_day, file.path(dp_dir, "XBTEUR_OHLC_one_day_xts.rds")))
+
+plot(one_min)
+plot()
+plot(one_day)
