6a381d19d1c88c81331da4b4088d5718d1825c1f
austin.haffenden <austin.haffenden@irstea.fr>
2018-04-17 15:48:21 +0200

    some progress towards mins. Needs lubridate for floor

--- R/to_period.R
+++ R/to_period.R
@@ -1,35 +1,61 @@
-to_period <- function(xts_in, period_in, out_dir, sub_period = 1) {
-  
-  curr_xts <-xts::to.period(xts_in, 
-                            period = period_in,
-                            k = sub_period)
-  
-  names(curr_xts) <- c("Open", "High", "Low", "Close", "Volume")
+create_index <- function(xts_in, period_in, sub_period) {
 
   if(period_in == "hour") {
     
     # fill the missing days with NAs
-    index <- seq(round(start(curr_xts), "hour")-1, 
-                 round(end(curr_xts), "hour")-1, by = "hour")
+    index <- seq(round(start(xts_in), "hour")-1, 
+                 round(end(xts_in), "hour")-1, by = "hour")
     
-    curr_xts_nas <- merge(curr_xts, zoo(, index))
+  } else if(period_in =="minutes") {
     
-    # fill the NAs with the previous non-NA value
-    curr_xts_nas_last <- na.locf(curr_xts_nas)
+    # fill the missing days with NAs
+    index <- seq(start(xts_in), end(xts_in), by = "day")
+  } 
+  
+  return(index)
+  
+  
+  
+}
+
+to_period <- function(xts_in, period_in, out_dir, sub_period = 1) {
+  
+  curr_ohlc <-xts::to.period(xts_in, 
+                            period = period_in,
+                            k = sub_period)
+  
+  names(curr_ohlc) <- c("Open", "High", "Low", "Close", "Volume")
+
+  index <- create_index(curr_ohlc, period_in, sub_period)
+  
+  # ensure that we don't duplicate the index with existing time
+  ind_diff <- as.character(index) %in% as.character(index(curr_ohlc))
+  new_index <- index[!ind_diff]
+   
+  curr_ohlc_nas <- merge(curr_ohlc, zoo(, new_index))
     
-    curr_xts_nas_last_59 <- lf_hourly_last[(.indexmin(curr_xts_nas_last)==59&
-                                              .indexsec(lf_hourly_last)==59),]
+  # fill the NAs with the previous non-NA value
+  curr_ohlc_nas_last <- na.locf(curr_ohlc_nas)
+  
+  if(period_in == "minutes") {
     
+    
+    
+  } else if(period_in == "hour") {  
+    curr_ohlc_nas_last <- curr_ohlc_nas_last[(floor(.indexmin(curr_ohlc_nas_last)==59)&
+                                              floor(.indexsec(curr_ohlc_nas_last)==59)),]
   } else if(period_in =="day") {
     
     # fill the missing days with NAs
-    index <- seq(start(curr_xts), end(curr_xts), by = "day")
+    index <- seq(start(curr_ohlc), end(curr_ohlc), by = "day")
     return(index)
-    curr_xts_nas <- merge(curr_xts, zoo(, index))
+    curr_ohlc_nas <- merge(curr_ohlc, zoo(, index))
     
     # fill the NAs with the previous non-NA value
-    curr_xts_nas_last <- na.locf(curr_xts_nas)
+    curr_ohlc_nas_last <- na.locf(curr_ohlc_nas)
     
   }
+    
+  return(curr_ohlc_nas_last)
     
 }
