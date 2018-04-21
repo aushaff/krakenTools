21c96e9d49a7becb45744b77ae124f4cbee54687
aushaff <haffamoto@yahoo.co.uk>
2018-04-21 16:25:21 +0200

    minor mod for to.day. appears to now be correct

--- R/to_period.R
+++ R/to_period.R
@@ -14,8 +14,8 @@ create_index <- function(xts_in, period_in, sub_period) {
     
   } else if(period_in == "day"){
     
-    index <- seq(lubridate::floor_date(start(xts_in), "day")+(3600*12)-1,
-                 lubridate::floor_date(end(xts_in), "day")+(3600*12)-1, by = "day")
+    index <- seq(lubridate::floor_date(start(xts_in), "day")+(3600*24)-1,
+                 lubridate::floor_date(end(xts_in), "day")+(3600*24)-1, by = "day")
   }
 }
 
@@ -25,25 +25,35 @@ to_period <- function(xts_in, period_in, out_dir, sub_period = 1) {
                             period = period_in,
                             k = sub_period)
   names(curr_ohlc) <- c("Open", "High", "Low", "Close", "Volume")
-
-  index <- create_index(curr_ohlc, period_in, sub_period)
   
+  index <- create_index(curr_ohlc, period_in, sub_period)
+
   # ensure that we don't duplicate the index with existing time
   ind_diff <- as.character(index) %in% as.character(index(curr_ohlc))
   new_index <- index[!ind_diff]
   curr_ohlc_nas <- merge(curr_ohlc, zoo(, new_index))
   
+  
   # fill the NAs with the previous non-NA value
   curr_ohlc_nas_last <- na.locf(curr_ohlc_nas)
-
+  
   if(period_in == "minutes") {
     
     curr_ohlc_nas_last <- curr_ohlc_nas_last[floor(.indexsec(curr_ohlc_nas_last))==59,]
     
-  } else if(period_in == "hour"||period_in =="day") {  
+  } else if(period_in == "hour") {  
+    
     curr_ohlc_nas_last <- curr_ohlc_nas_last[(floor(.indexmin(curr_ohlc_nas_last))==59&
                                               floor(.indexsec(curr_ohlc_nas_last))==59),]
-  } 
+
+  } else if(period_in =="day") {
+    
+    curr_ohlc_nas_last <- curr_ohlc_nas_last[(.indexhour(curr_ohlc_nas_last)==23&
+                                                floor(.indexmin(curr_ohlc_nas_last))==59&
+                                                floor(.indexsec(curr_ohlc_nas_last))==59),]
+    
+    
+  }
     
   return(curr_ohlc_nas_last)
     
