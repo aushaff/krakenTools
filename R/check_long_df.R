67fcc13c9a0aa2dedde81c9c61dc3840494674b4
aushaff <aushaff@zoho.eu>
2019-10-13 20:18:44 +0200

    duplication check removed as unnecesary

--- R/check_long_df.R
+++ R/check_long_df.R
@@ -32,7 +32,7 @@ check_long_df <- function(curr_raw_dir_in,
   #=============================================================
   # check for existing metadata file
   curr_meta_filename <- list.files(curr_dat_prod_dir_in,
-                                   pattern = "long_meta_data")
+                                   pattern = "long_metadata")
   
   if(length(curr_meta_filename)>0) {
     curr_meta_since <- strsplit(curr_meta_filename, 
@@ -66,14 +66,19 @@ check_long_df <- function(curr_raw_dir_in,
                      curr_error_since == 999999, 
                      TRUE,
                      FALSE)
-  
+  # print(curr_long_since)
+  # print(curr_meta_since)
+  # print(curr_error_since)
+  # print(bool)
+  # print(bool_alt)
+
   if(bool==TRUE|bool_alt==TRUE) {
     return("Nothing to check")
   }
   
   #============================================================================
   # After here it is just the current check_long process
-  
+  print("...reading current long_df...")
   # take in the long_df
   curr_long_df <- readr::read_delim(file.path(curr_dat_prod_dir_in,
                                               curr_long_filename),
@@ -81,7 +86,7 @@ check_long_df <- function(curr_raw_dir_in,
   
   # all the files in the raw directory
   all_files <- list.files(curr_raw_dir_in)
- 
+  
   #=============================================================
   # Need to find which raw files have been compared and which haven't
   curr_checked_filename <- list.files(curr_dat_prod_dir_in,
@@ -95,6 +100,11 @@ check_long_df <- function(curr_raw_dir_in,
     
     # where the last checked file sits in all_files
     lc_ind <- match(last_checked_file, all_files)
+    
+    if(is.na(lc_ind)) {
+      stop("metadata since not in files")
+    }
+    #print(lc_ind)
     
     # if the index of the last checked file is the same as the number of 
     # raw files all current files have been checked
@@ -114,18 +124,29 @@ check_long_df <- function(curr_raw_dir_in,
   } else {
     return("Incorrect number of metadata files present")
   }
- 
+  #print(files_to_check)
+  
   #=====================
-  # find duplicated rows - we are still checking the whole long here
-  print("...checking duplicates...")
-  dup_inds <- which(duplicated(curr_long_df) | 
-                      duplicated(curr_long_df, fromLast = TRUE))
+  # This is skipped as should be included in file check
+  # # find duplicated rows - we are still checking the whole long here
+  # print("...checking duplicates...")
+  # 
+  # curr_long_sub_dt <- data.table(curr_long_df[,c("price", "volume", "unix_time")])
+  # temp_dups <- duplicated(curr_long_sub_dt)
+  # stop()  
+  # 
+  # dup_inds <- which(duplicated(curr_long_df) | 
+  #                     duplicated(curr_long_df, fromLast = TRUE))
+  # 
+  # print(1)
+  # dup_rows <- curr_long_df[dup_inds, ]
+  # print(2)
+  # if(nrow(dup_rows)>0) {
+  #   dup_rows$flag <- "DUPLICATED"
+  # }
+  # #print(dup_inds)
+  # #print(dup_rows)
   
-  dup_rows <- curr_long_df[dup_inds, ]
-  
-  if(nrow(dup_rows)>0) {
-    dup_rows$flag <- "DUPLICATED"
-  }
   #============================================================
   # find rows from the raw data that don't exist in the long_df
   # find all the files
@@ -141,9 +162,12 @@ check_long_df <- function(curr_raw_dir_in,
                            delim = ";")
     
     not_in_long <- dplyr::anti_join(curr_dat, curr_long_df)
+    rm(curr_dat)
+    gc()
     
     return(not_in_long)
   }))
+  #print(rows_not_in_long)
   
   if(nrow(rows_not_in_long)>0) {
     
@@ -154,35 +178,49 @@ check_long_df <- function(curr_raw_dir_in,
   #Create metadata or error files
   print("...binding out information...")
   out_df <- rbind(dup_rows, rows_not_in_long)
-  
+  print(out_df)
+
   if(nrow(out_df)>0) { # this is if there are duplicates or 
                       # missing rows in long_df
-    
-   error_filename <- paste0(strsplit(curr_long_filename, "_long")[[1]][1],
-                            "_check_ERROR_df.csv")
-    
-   readr::write_delim(out_df, file.path(curr_dat_prod_dir_in, 
-                                 error_filename),
-               delim = ";")
-    
-  } else {
-    
-    checked_filename <- paste0(strsplit(curr_long_filename, "_long")[[1]][1],
-                               "_long_metadata_df.csv")
-    
-    last_date <- strsplit(curr_long_filename, "__")[[1]][1]
-    n_rows <- nrow(curr_long_df)
-    date_checked <- Sys.time()
-    
-    out_df <- data.frame(last_date = last_date,
-                         n_rows = n_rows,
-                         date_checked = date_checked)
-    
-    readr::write_delim(out_df, file.path(curr_dat_prod_dir_in, 
-                                  checked_filename),
-                delim = ";")
-   }
   
-  return("Long dataframe updated")
->>>>>>> cceaf8a51348a4833e7f740cdebdb04d9828808d
+    print("...writing error file...")
+    error_filename <- paste0(strsplit(curr_long_filename, "_long")[[1]][1],
+                             "_check_ERROR_df.csv")
+
+    readr::write_delim(out_df, file.path(curr_dat_prod_dir_in,
+                                         error_filename),
+                       delim = ";")
+    
+    if((length(curr_error_filename)>0)) {
+      
+      file.remove(file.path(curr_dat_prod_dir_in, curr_error_filename))
+      
+    }
+  
+  } 
+  
+  print("...writing metadata file...")
+  checked_filename <- paste0(strsplit(curr_long_filename, "_long")[[1]][1],
+                             "_long_metadata_df.csv")
+  
+  last_date <- strsplit(curr_long_filename, "__")[[1]][1]
+  n_rows <- nrow(curr_long_df)
+  date_checked <- Sys.time()
+  
+  out_df <- data.frame(last_date = last_date,
+                       n_rows = n_rows,
+                       date_checked = date_checked)
+  
+  readr::write_delim(out_df, file.path(curr_dat_prod_dir_in, 
+                                checked_filename),
+              delim = ";")
+  
+  if((length(curr_meta_filename)>0)) {
+    
+    file.remove(file.path(curr_dat_prod_dir_in, curr_meta_filename))
+    
+  }
+  
+return("Long dataframe checked")
+
 }
