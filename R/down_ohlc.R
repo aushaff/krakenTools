af6a228ed69c2f3361a1418c55d54a64c15ab8bb
austin haffenden <aushaff@zoho.eu>
2024-02-23 17:09:19 +0100

    resinstated and updated as package

---
+++ R/down_ohlc.R
@@ -0,0 +1,12 @@
+#'@title download ohlc for an asset pair
+#'@description download ohlc between specified dates, of a specified period for
+#'the specified asset pair
+#'@export
+down_ohlc <- function(asset_pair,
+                      min_date,
+                      max_date,
+                      interval) {
+
+
+
+}
